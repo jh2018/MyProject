46f688c45db7898a5fce761938de3bff4fc2bf72
jh2008 <2423914@qq.com>
2017-01-17 10:28:44 +0800

    错错错

--- Solution1/ClassLibrary1/Class1.cs
+++ Solution1/ClassLibrary1/Class1.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Common
+namespace ClassLibrary1
 {
-    public class Git
+    public class Class1
     {
-        
-        public Git()
+        public Class1()
         {
-
         }
     }
 }
--- Solution1/ConsoleApp1/Program.cs
+++ Solution1/ConsoleApp1/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Common
+namespace ConsoleApp1
 {
-    public class Git
+    public class Program
     {
-        
-        public Git()
+        public static void Main(string[] args)
         {
-
+            Console.Write("hello world");
         }
     }
 }
